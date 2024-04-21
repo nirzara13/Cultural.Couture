6f0fdd5f6dd58bf3802444d1e925250ff0cd1397
Nirzara <145440724+nirzara13@users.noreply.github.com>
2024-04-21 02:31:07 +0200

    add folder vue

---
+++ vue/vueConfirmation.Inscription.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE-edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="icon" href="../photos/Logo_Cultural_Couture.png" type="image/x-icon">
+    <link rel="shortcut icon" href="../photos/Logo_Cultural_Couture.png" type="image/x-icon">
+
+    <title>Confirmation Inscription</title>
+    <link rel="stylesheet" href="">
+    <style>
+        /* Notre style CSS ici */
+   
+*{
+margin:0;
+padding:0;
+box-sizing: border-box;
+font-family:'Poppins', sans-serif;
+}
+body{
+background-color: #d0bdde;
+display: flex;
+align-items: center;
+justify-content: center;
+height:100vh;
+}
+form{
+display: flex ;
+flex-direction: column;
+background-color: #fff;
+padding: 40px;
+border-radius: 6px;
+box-shadow: 0 0 10px  rgba(0,0,0,0.2);
+}
+h4{
+text-align: center;
+font-size: 20px;
+}
+hr{
+margin: 10px 0;
+background-color: #ccc;
+border:0;
+height: 1px;
+width: 100%;
+}
+.name-field {
+display: flex;
+width: 60%;
+justify-content: space-between;
+}
+.name-field div {
+display: flex;
+flex-direction: column;
+}
+.name-field div {
+width: 49%;
+}
+label{
+margin-bottom: 10px;
+}
+input{
+margin-bottom: 5px;
+padding:5px;
+outline:0;
+border: 1px solid rgba(0,0,0,0.4);
+}
+input:focus{
+border: 1px solid #000000;
+}
+
+input[type="submit"]{
+margin-top: 15px;
+background-color: #000000;
+color:#fff;
+border:1px solid #000000;
+cursor: pointer;
+}
+
+  
+p {
+text-align: center;
+margin: 5px 0;
+font-size: 14px;
+}
+p  a {
+text-decoration: 0;
+color:#460079;
+}
+
+p  a:hover {
+text-decoration: underline;
+}
+    </style>
+</head>
+<body>
+
+<main>
+<h1>Inscription effectuée!</h1>
+<br>
+<br>
+
+<h2>Veuillez vous <a href=./?action=connexion>connecter</a>.
+
+</main>
+
+</body>
+</html>
