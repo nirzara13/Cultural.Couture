52cbf71c0414fb785238e0fbe2422540b422e309
Nirzara <145440724+nirzara13@users.noreply.github.com>
2024-04-23 22:27:44 +0200

    Update accueil.php

--- controleur/accueil.php
+++ controleur/accueil.php
@@ -4,10 +4,6 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
     $racine="..";
 }
 
-// include_once "$racine/modele/bd.resto.inc.php";
-// include_once "$racine/modele/bd.typecuisine.inc.php";
-// include_once "$racine/modele/bd.photo.inc.php";
-
 
 
 $titre = "Accueil - Cultural Couture.fr";
