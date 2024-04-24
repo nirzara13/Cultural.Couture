029695e690f543dbb066a06321486fd5a260d9f7
Nirzara <145440724+nirzara13@users.noreply.github.com>
2024-04-24 21:36:36 +0200

    Update monProfil.php

--- controleur/monProfil.php
+++ controleur/monProfil.php
@@ -5,14 +5,14 @@ if ( $_SERVER["SCRIPT_FILENAME"] == __FILE__ ){
 include_once "$racine/modele/authentification.inc.php";
 include_once "$racine/modele/bd.utilisateur.inc.php";
 
-// recuperation des donnees GET, POST, et SESSION
+// récuperation des données GET, POST, et SESSION
 
-// appel des fonctions permettant de recuperer les donnees utiles a l'affichage 
+// appel des fonctions permettant de récuperer les donnees utiles pour l'affichage 
 if (isLoggedOn()){
     $mailU = getemailLoggedOn();
     $util = getUtilisateurByemail($mailU);
     
-    // appel du script de vue qui permet de gerer l'affichage des donnees
+    // appel du script de vue qui permet de gérer l'affichage des données
     $titre = "Mon profil";
     include "$racine/vue/vueMonProfil.php";
     
