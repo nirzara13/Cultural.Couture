6b078cee90c97537e89ae860b79d70fbd82689f7
Nirzara <145440724+nirzara13@users.noreply.github.com>
2024-04-24 21:54:11 +0200

    Update rechercher.php

--- controleur/rechercher.php
+++ controleur/rechercher.php
@@ -1,7 +1,7 @@
 <?php
 require_once "../modele/bd.inc.php";
 
-// Fonction pour gérer l'action de recherche
+
 function gererAction($action) {
     // Vérifiez l'action demandée et effectuez les opérations nécessaires
     if ($action === "recherche") {
@@ -9,7 +9,7 @@ function gererAction($action) {
         $motCle = isset($_GET['recherche']) ? $_GET['recherche'] : "";
         $resultats = rechercher($motCle);
 
-        // Si des résultats sont trouvés, les retourner pour affichage sur la même page
+        // Si des résultats sont trouvés, les retourner afin de les afficher sur la même page
         if (!empty($resultats)) {
             return afficherResultats($resultats);
         } else {
@@ -19,31 +19,31 @@ function gererAction($action) {
         }
     }
 
-    // Si l'action n'est pas gérée ici, retournez une chaîne vide
+    // Si l'action n'est pas géré ici, retournez une chaîne vide
     return "";
 }
 
-// Fonction pour rechercher dans la base de données
+
 function rechercher($motCle){
     try {
         // Connexion à la base de données
         $conn = connexionPDO();
 
-        // Préparer la requête SQL pour rechercher dans la table Tenues
-        $stmtTenues = $conn->prepare("SELECT t.*, p.cheminP FROM Tenues t LEFT JOIN photo p ON t.Id_Tenue = p.idTenue WHERE t.NomTenue LIKE :motCle");
-        $stmtTenues->bindValue(':motCle', '%' . $motCle . '%', PDO::PARAM_STR);
-        $stmtTenues->execute();
+        // requête SQL pour rechercher dans la table Tenues
+        $searchTenues = $conn->prepare("SELECT Tenues.*, photo.cheminP FROM Tenues LEFT JOIN photo ON Tenues.Id_Tenue = photo.idTenue WHERE Tenues.NomTenue LIKE :motCle");
+        $searchTenues->bindValue(':motCle', '%' . $motCle . '%', PDO::PARAM_STR);
+        $searchTenues->execute();
         
         // Récupérer les résultats de la recherche pour les tenues
-        $resultatsTenues = $stmtTenues->fetchAll(PDO::FETCH_ASSOC);
+        $resultatsTenues = $searchTenues->fetchAll(PDO::FETCH_ASSOC);
         
-        // Préparer la requête SQL pour rechercher dans la table Accessoires
-        $stmtAccessoires = $conn->prepare("SELECT a.*, p.cheminP FROM Accessoires a LEFT JOIN photo p ON a.Id_Accessoire = p.idAccessoire WHERE a.NomAccessoire LIKE :motCle");
-        $stmtAccessoires->bindValue(':motCle', '%' . $motCle . '%', PDO::PARAM_STR);
-        $stmtAccessoires->execute();
+        // requête SQL pour rechercher dans la table Accessoires
+        $searchAccessoires = $conn->prepare("SELECT Accessoires.*, photo.cheminP FROM Accessoires LEFT JOIN photo ON Accessoires.Id_Accessoire = photo.idAccessoire WHERE Accessoires.NomAccessoire LIKE :motCle");
+        $searchAccessoires->bindValue(':motCle', '%' . $motCle . '%', PDO::PARAM_STR);
+        $searchAccessoires->execute();
         
         // Récupérer les résultats de la recherche pour les accessoires
-        $resultatsAccessoires = $stmtAccessoires->fetchAll(PDO::FETCH_ASSOC);
+        $resultatsAccessoires = $searchAccessoires->fetchAll(PDO::FETCH_ASSOC);
         
         // Fermer la connexion à la base de données
         $conn = null;
